adaee9ffbf33ac554453418470904c8f990dba9c
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2023-11-13 15:58:35 +1100

    add localhost run script

---
+++ run-localhost.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Stop the nginx container if it exists
+if docker ps -a --format '{{.Names}}' | grep -q '^nginx$'; then
+    docker stop nginx
+fi
+
+# Run your build script
+./npm-build.sh
+
+# Run Nginx container
+docker run --rm -d -p 80:80 -v $(pwd)/app/public:/usr/share/nginx/html --name nginx nginx
